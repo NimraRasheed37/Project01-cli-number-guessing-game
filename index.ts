b14b5ca9b8be2f9696d93b53207e99b06888d737
Nimra37PK <nimrarasheed.na@gmail.com>
2024-06-04 09:32:06 +0500

    added welcpme message

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@
 import inquirer from "inquirer"
 
 const randomNumber = Math.floor(Math.random()* 10 +1);
+console.log("Welcome to my Number Guessing game");
+
 const answers = await inquirer.prompt([{
     message: "Please guess a number between 1 to 10:",
     type: "number",
